04bddafb061ed0cde3c1e27e2231ec7eedb6d94c
priya <priya.patel@tothenew.com>
2020-04-08 16:59:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetpackexercise1/DB/FavouriteDbHelper.java
@@ -0,0 +1,28 @@
+package com.example.jetpackexercise1.DB;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FavouriteDbHelper extends SQLiteOpenHelper {
+
+    public FavouriteDbHelper(Context context) {
+        super(context, DbSettings.DB_NAME, null, DbSettings.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + DbSettings.DBEntry.TABLE + " ( " +
+                DbSettings.DBEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                DbSettings.DBEntry.COL_FAV_URL + " TEXT NOT NULL, " +
+                DbSettings.DBEntry.COL_FAV_DATE + " INTEGER NOT NULL);";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DbSettings.DBEntry.TABLE);
+        onCreate(db);
+    }
+
+}
